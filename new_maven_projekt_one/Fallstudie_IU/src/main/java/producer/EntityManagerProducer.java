4f04612591331079f8400a134e28652afd3cc3a1
smilenow789 <187426292+smilenow789@users.noreply.github.com>
2025-06-07 15:58:24 +0200

    change some logic on EntityManagerProducer,java

--- new_maven_projekt_one/Fallstudie_IU/src/main/java/producer/EntityManagerProducer.java
+++ new_maven_projekt_one/Fallstudie_IU/src/main/java/producer/EntityManagerProducer.java
@@ -6,45 +6,29 @@ import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence; // <-- Import this!
-// import jakarta.persistence.PersistenceUnit; // <-- We'll remove this field annotation
+import jakarta.persistence.Persistence;
 
 @ApplicationScoped
 public class EntityManagerProducer {
 
-	// REMOVE the @PersistenceUnit annotation from here.
-	// Instead, we will create the EntityManagerFactory explicitly in a producer
-	// method.
-	// private EntityManagerFactory emf; // Keep this field, but it won't be
-	// auto-injected by Tomcat's limited JPA support
-
-	// This method produces the EntityManagerFactory for injection.
-	// It's crucial for Tomcat as it doesn't auto-inject @PersistenceUnit
 	@Produces
-	@ApplicationScoped // EntityManagerFactory should be application-scoped
+	@ApplicationScoped
 	public EntityManagerFactory createEntityManagerFactory() {
-		// "fallstudie" must match the persistence-unit name in persistence.xml
 		return Persistence.createEntityManagerFactory("fallstudie");
 	}
 
-	// This method injects the EntityManagerFactory created above
-	// and then produces an EntityManager instance.
 	@Produces
-	@RequestScoped // EntityManager should be request-scoped
-	public EntityManager createEntityManager(EntityManagerFactory emf) { // emf is now INJECTED here
-		// This line (line 20) will now work because emf is guaranteed to be initialized
+	@RequestScoped
+	public EntityManager createEntityManager(EntityManagerFactory emf) {
 		return emf.createEntityManager();
 	}
 
-	// This method cleans up the EntityManager when it's no longer needed
 	public void closeEntityManager(@Disposes EntityManager em) {
 		if (em.isOpen()) {
 			em.close();
 		}
 	}
 
-	// This method disposes of the EntityManagerFactory when the application shuts
-	// down
 	public void closeEntityManagerFactory(@Disposes EntityManagerFactory emf) {
 		if (emf.isOpen()) {
 			emf.close();
